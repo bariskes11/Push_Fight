d9ab6af6747e47d287778abe72290e6e721043f1
bariskes11 <kesdesng@gmail.com>
2021-03-13 07:03:46 +0300

    Kamera Hareketlerinin hazırlanması

---
+++ Rope_Fight/Assets/Scripts/CameraMovements.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CameraMovements : MonoBehaviour
+{
+    public int Focused_Player_Index = 0;
+    private Game_Play_Manager gm = new Game_Play_Manager();
+    public Vector3 playerOffset;
+    private Vector3 PlayerPosition;
+    private GameObject playerObj;
+    public int CurrentPlayerIndex;
+    private PlayerIndexer[] PlayersList;
+    private GameObject currentPlayerToZoom;
+    public bool QuestionTime = false;
+    public bool QuestionResulted = false;
+    Camera c;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = FindObjectOfType<Game_Play_Manager>();
+        c = Camera.main;
+        CurrentPlayerIndex = 0;
+        PlayersList = FindObjectsOfType<PlayerIndexer>();
+        playerObj = PlayersList.Where(x => x.PlayerIndex == CurrentPlayerIndex).FirstOrDefault().gameObject;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (gm.GameStarted)
+        {
+            CameraPositioning();
+        }
+    }
+    void CameraPositioning()
+    {
+        if (gm.Game_Finished)
+        {
+            transform.LookAt(playerObj.transform);
+            transform.Translate(Vector3.right * Time.deltaTime);
+            return;
+        }
+        PlayerPosition = Vector3.MoveTowards(c.transform.position, playerObj.transform.position + playerOffset, .1F);
+        //PlayerPosition = playerObj.transform.position + playerOffset;
+        if (c.transform.position == playerObj.transform.position + playerOffset && !QuestionTime)
+        {
+            AskQuestion();
+        }
+        c.transform.position = PlayerPosition;
+    }
+    void AskQuestion()
+    {
+        QuestionTime = true;
+        Debug.Log("Soru Geldi");
+        StartCoroutine(WaitForAnswerTimeout());
+    }
+
+    IEnumerator WaitForAnswerTimeout()
+    {
+        yield return new WaitForSeconds(5F);
+        CurrentPlayerIndex++;
+        if (CurrentPlayerIndex > PlayersList.Count()-1)
+        {
+            CurrentPlayerIndex =0;
+        }
+        playerObj = PlayersList.Where(x => x.PlayerIndex == CurrentPlayerIndex).FirstOrDefault().gameObject;
+        QuestionTime = false;
+    }
+}
+
+
+
+
+
+
---
+++ Rope_Fight/Assets/Scripts/Game_Play_Manager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Play_Manager : MonoBehaviour
+{
+    public bool GameStarted = false;
+    public bool Game_Finished = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+    public void StartGame()
+    {
+        GameStarted = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
