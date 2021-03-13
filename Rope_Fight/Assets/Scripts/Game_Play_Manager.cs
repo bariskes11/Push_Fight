613eac2445e594e872f550f4c1652810b95086b8
bariskes11 <kesdesng@gmail.com>
2021-03-13 14:31:28 +0300

    asset Düzenlemeleri kod iyileştirmeleri

--- Rope_Fight/Assets/Scripts/Game_Play_Manager.cs
+++ Rope_Fight/Assets/Scripts/Game_Play_Manager.cs
@@ -1,27 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Game_Play_Manager : MonoBehaviour
 {
     public bool GameStarted = false;
     public bool Game_Finished = false;
     public GameObject gameStartPanel;
+    private List<OverLapChecker> GenerateTotalList;
+    public Text CurentFillAmount;
+    public float CurrentTimeLeft;
+
+    public float CurrentOveralAmount;
     // Start is called before the first frame update
     void Start()
     {
-        
-        
+        ShowChallenge();
+
+
     }
     public void StartGame()
     {
         GameStarted = true;
         gameStartPanel.SetActive(false);
     }
-
+    public void ShowChallenge()
+    {
+        GenerateTotalList = GameObject.FindObjectsOfType<OverLapChecker>().ToList(); 
+       
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if(GameStarted)
+        CurrentOveralAmount = GenerateTotalList.Sum(x => x.overlapAmount) / GenerateTotalList.Count;
+    }
+
+    public void SetTotalAmounth()
+    {
+        CurrentOveralAmount = GenerateTotalList.Sum(x => x.overlapAmount) / GenerateTotalList.Count;
+        CurentFillAmount.text = CurrentOveralAmount.ToString("###.##")+ " %";
     }
 }
--- Rope_Fight/Assets/Scripts/OverLapChecker.cs
+++ Rope_Fight/Assets/Scripts/OverLapChecker.cs
@@ -6,10 +6,12 @@ public class OverLapChecker : MonoBehaviour
 {
     public float maxDistance = 50F;
     public float mindistance = 10F;
+    public Game_Play_Manager gm = new Game_Play_Manager();
     private Transform obj_pos;
     private void Start()
     {
         obj_pos = transform;
+        gm = GameObject.FindObjectOfType<Game_Play_Manager>();
     }
     float overlapDistance;
    public float overlapAmount;
@@ -18,36 +20,36 @@ public class OverLapChecker : MonoBehaviour
         overlapAmount = Vector3.Distance(collision.gameObject.transform.position, this.transform.position);
         overlapAmount = 100 - overlapAmount;
         Debug.Log("iki Obje Arası uzaklık" + overlapAmount);
+        gm.SetTotalAmounth();
     }
-    void LateUpdate() // otherwise, visual lag can make for inconsistent collision checking.
-    {
-        Collider2D[] cols = Physics2D.OverlapPointAll(
-                Vector3.Scale(new Vector3(1, 1, 0), obj_pos.position), // background at z=0 
-                LayerMask.GetMask("background")); // ignore non background layer objects
-        Collider2D backgroundCol = null;
+    //void LateUpdate() // otherwise, visual lag can make for inconsistent collision checking.
+    //{
+    //    Collider2D[] cols = Physics2D.OverlapPointAll(
+    //            Vector3.Scale(new Vector3(1, 1, 0), obj_pos.position), // background at z=0 
+    //            LayerMask.GetMask("background")); // ignore non background layer objects
+    //    Collider2D backgroundCol = null;
+    //    for (int i = 0; i < cols.Length; i++) // order of increasing z value
+    //    {
+    //        if (cols[i].tag == "Background") // may be redundant with layer mask
+    //        {
+    //            backgroundCol = cols[i];
+    //            break;
+    //        }
+    //    }
+    //    if (backgroundCol != null)
+    //    {
+    //        overlapDistance = Vector3.Distance(this.transform.position, backgroundCol.gameObject.transform.position);
+            
+    //        //   float CalcDistance = Mathf.Clamp(overlapDistance, mindistance, maxDistance);
+    //        float x = mindistance * 100 / overlapDistance;
+    //        Debug.Log(x);
 
-        for (int i = 0; i < cols.Length; i++) // order of increasing z value
-        {
-            if (cols[i].tag == "Background") // may be redundant with layer mask
-            {
-                backgroundCol = cols[i];
-                break;
-            }
-        }
-        if (backgroundCol != null)
-        {
-            overlapDistance = Vector3.Distance(this.transform.position, backgroundCol.gameObject.transform.position);
-            Debug.Log("DETECTSBACKGROUND: " + backgroundCol.name + " Distance:" + overlapDistance);
-            //   float CalcDistance = Mathf.Clamp(overlapDistance, mindistance, maxDistance);
-            float x = mindistance * 100 / overlapDistance;
-            Debug.Log(x);
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("Does not detect Background");
+    //    }
 
-        }
-        else
-        {
-            Debug.Log("Does not detect Background");
-        }
-
-    }
+    //}
 
 }
