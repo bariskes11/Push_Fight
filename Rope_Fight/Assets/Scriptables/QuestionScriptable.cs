2097dc75b866a5fccd1cf5b9e9c5a4caa9e70081
bariskes11 <kesdesng@gmail.com>
2021-03-14 00:24:32 +0300

    Soru Sablonlari Eklendi

---
+++ Rope_Fight/Assets/Scriptables/QuestionScriptable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="SoruSablonu",menuName ="ScriptableObjects/SoruSablonuOlustur",order =1)]
+public class QuestionScriptable : ScriptableObject
+{
+    public Sprite[] shapes_dotted;
+    public Sprite[] shapes_Normal;
+}
---
+++ Rope_Fight/Assets/Scripts/QuestionCreator.cs
@@ -0,0 +1,39 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class QuestionCreator : MonoBehaviour
+{
+    public QuestionScriptable soruSablonu;
+    private SpriteRenderer[] spritesInChildren;
+    private List<int> selectedIndexes = new List<int>();
+    private void Start()
+    {
+        spritesInChildren = this.GetComponentsInChildren<SpriteRenderer>();
+        GenerateRandom();
+    }
+    void GenerateRandom()
+    {
+        for (int j = 0; j < spritesInChildren.Length; j++)
+        {
+            int Rand = Random.Range(0, spritesInChildren.Length);
+            while (selectedIndexes.Contains(Rand))
+            {
+                Rand = Random.Range(0, spritesInChildren.Length);
+            }
+            selectedIndexes.Add(Rand);
+        }
+    }
+    public void CreateQuestion()
+    {
+        int i = 0;
+        foreach (var item in spritesInChildren)
+        {
+            item.sprite = soruSablonu.shapes_Normal[selectedIndexes.ElementAt(i)];
+            i ++;
+        }
+    }
+
+}
