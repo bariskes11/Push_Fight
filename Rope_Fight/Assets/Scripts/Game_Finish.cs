a50ef19a08d83c12a0232e99675bc83ba07e2bc5
bariskes11 <kesdesng@gmail.com>
2021-03-15 23:01:25 +0300

    Game Finish mekanizmalarının çalışılmaya başlanması

---
+++ Rope_Fight/Assets/Scripts/Game_Finish.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Finish : MonoBehaviour
+{
+
+    public Game_Play_Manager gm;
+     void Start()
+    {
+        gm = FindObjectOfType<Game_Play_Manager>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            // show lossse panel
+            Debug.Log(" You Lost");
+        }
+        else if (other.gameObject.tag == "Enemy")
+        {
+            Debug.Log(" You Win");
+            // show win panel
+        }
+    }
+}
